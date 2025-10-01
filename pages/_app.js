7951c365f3295838a5742e8a427fc52b8e48cb80
Juliasuarzzz <juliasuarezzz9@gmail.com>
2025-10-01 21:16:27 +0200

    perima parte acabada empezado about me

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,30 @@
 import '../styles/globals.less'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { useEffect } from 'react'
+import { Poppins } from 'next/font/google'
+import Script from 'next/script' // 👈 Importa Script
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['300', '400', '700'],
+})
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
     import('bootstrap/dist/js/bootstrap.bundle.min.js')
   }, [])
 
-  return <Component {...pageProps} />
+  return (
+    <main className={poppins.className}>
+      {/* Aquí cargas el kit de Font Awesome */}
+      <Script
+        src="https://kit.fontawesome.com/0b0bc56781.js"
+        crossOrigin="anonymous"
+        strategy="beforeInteractive"
+      />
+      <Component {...pageProps} />
+    </main>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import styles from '../styles/Home.less'
 import { initIntroAnimations } from '../lib/introAnimations'; 
 import { paginageneral } from '../lib/paginageneral'; 
 import TextType from '../components/TextType.jsx';
+import ShinyText from '../components/ShinyText.jsx';
 
 export default function Intro() {
   useEffect(() => {
@@ -37,6 +38,7 @@ export default function Intro() {
         <div className="box-link">
           <img src="img/icon-web.svg" alt="fondo" />
           <span className="text">heloooooo</span>
+          <div class="glow"></div>
         </div>
       </div>
 
@@ -90,13 +92,18 @@ export default function Intro() {
 
     {/* inicio pagina inicial */}
     <div className="pagina-inicio">
+
+      {/* Inicio primera pagina */}
+
       <div className="container-pagina-inicio">
         <div className="box-desc-inicio">
 
           <div className="box-innovate">
-            <div class="glow"></div>
-            <img src="img/star.png" alt="fondo" />
-            <span>Ready to innovate</span>
+            <div className="innovate-item">
+              <div className="glow"></div>
+              <img src="img/star.png" alt="fondo" />
+              <span>Ready to innovate</span>
+            </div>
           </div>
 
           <div className="text-inicio">
@@ -122,12 +129,83 @@ export default function Intro() {
             </main>
           </div>
 
+          <div className="desc-info-inicio">
+            <span>Designing Modern, Efficient, and Intuitive Web Experiences to Solve Real-World Digital Challenges</span>
+          </div>
+
+          <div className="box-lenguaje-inicio">
+            <div className="lenguaje-item"><ShinyText text="CSS" disabled={false} speed={3} className='custom-class' /></div>
+            <div className="lenguaje-item"><ShinyText text="JavaScript" disabled={false} speed={3} className='custom-class' /></div>
+            <div className="lenguaje-item"><ShinyText text="SQL" disabled={false} speed={3} className='custom-class' /></div>
+            <div className="lenguaje-item"><ShinyText text="PHP" disabled={false} speed={3} className='custom-class' /></div>
+          </div>
+
+          <div className="box-links-inicio">
+            <div className="link-item">
+              Projects
+              <i class="fa-solid fa-arrow-up-right-from-square"></i>
+              <div class="glow"></div>
+            </div>
+            <div className="link-item">
+              Contact
+              <i class="fa-regular fa-envelope"></i>
+              <div class="glow"></div>
+            </div> 
+
+          </div>
+
+          <div className="box-redes-inicio">
+              <a className="redes-item">
+                <img src="img/github.svg" alt="fondo" />
+                <div class="glow"></div>
+              </a>
+              <a className="redes-item">
+                <img src="img/linkedin.svg" alt="fondo" />
+                <div class="glow"></div>
+              </a>
+              <a className="redes-item">
+                <img src="img/inst.svg" alt="fondo" />
+                <div class="glow"></div>
+              </a>
+          </div>
+
         </div>
 
+        
+
         <div className="box-img-inicio">
-          <canvas id="dotlottie-canvas" style={{ width: "300px", height: "300px" }}></canvas>
+          <canvas id="dotlottie-canvas" style={{ width: "400px", height: "400px" }}></canvas>
         </div>
         
+      </div>
+
+      {/* Fin primera pagina */}
+
+      {/* Inicio about me */}
+      <div className="pagina-about-me">
+
+        <div className="container-pagina-about-me">
+
+          <div className='title-about-me'>
+            <h2>About Me</h2>
+            <div>
+              <img src="img/star.png" alt="fondo" />
+              <span>Transforming ideas into digital experiences</span>
+              <img src="img/star.png" alt="fondo" />
+            </div>
+          </div>
+
+          <div className="info-box-about-me">
+                <div className="box-img-about-me">
+                  
+                </div>
+                <div className="box-desc-about-me">
+                  <h2 className='gradient'>Hello I&apos;m</h2>
+                  <h2>Julia Suarez</h2>
+                  <span> a passionate Frontend Developer dedicated to crafting engaging and user-friendly web experiences. With a strong foundation in HTML, CSS, and JavaScript, I specialize in creating responsive designs that adapt seamlessly across devices. My journey in web development began with a fascination for how websites are built, leading me to pursue formal education and hands-on projects. I thrive on solving complex problems and continuously learning new technologies to stay ahead in this ever-evolving field. When I&apos;m not coding, you&apos;ll find me exploring the latest design trends or contributing to open-source projects. Let&apos;s connect and create something amazing together!</span>
+                </div>
+          </div>
+        </div>
       </div>
     </div>
 
